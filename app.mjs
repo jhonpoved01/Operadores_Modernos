c298e69ce69f2fa6b1c3787b600a0baafcd6a415
saray-cod <puertolinni@gmail.com>
2026-04-17 18:24:51 -0500

    feat(apropiacion): se agrega correctamente ejercicio 1 transferencia.closes #1

--- app.mjs
+++ app.mjs
@@ -7,8 +7,9 @@
 import confiFinal from './transferencia/eje_5.js';
 import { procesarPedido } from './apropiacion/ejer_1.mjs';
 const pedido1 = procesarPedido(
-    {cliente: 'saray', producto: 'Crochet kit', cantidad: 2},
+    {cliente: 'saray', producto: 'Crochet kit', cantidad: 3},
     'agujas extras',
-    'lana negra'
+    'lana negra',
+    'patrón avanzado'
 );
 console.log(pedido1);
